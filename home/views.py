d3213b078879ac55c985d1fac4081d1be0555678
Ayush Gupta <ayush.keshav2004@gmail.com>
2022-03-22 03:25:10 +0530

    bug-edit-2

--- home/views.py
+++ home/views.py
@@ -328,7 +328,8 @@ def ip_data(request):
     # Setting up the handler.
     handler = ipinfo.getHandler(access_token)
     
-    print(os.environ.get("IPINFO_KEY") + "\t:\t" + handler)
+    print(os.environ.get("IPINFO_KEY"))
+    print(handler)
 
     # Getting the Data from IPInfo
     data = handler.getDetails()
